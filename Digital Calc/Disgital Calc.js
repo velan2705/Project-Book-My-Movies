eb6165d5051fc2a8285ac9cb2b5ac34c027a755a
velan2705 <126846126+velan2705@users.noreply.github.com>
2023-04-16 21:29:45 -0700

    Add files via upload
    Digital calc 
    By using 
    Html , Css and Java Script

---
+++ Digital Calc/Disgital Calc.js
@@ -0,0 +1,69 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML +="%";
+  }
+  function myFunction1() {
+    document.getElementById("demo").innerHTML +=7;    
+  }
+  function myFunction2() {
+    document.getElementById("demo").innerHTML +=8;
+  }
+  function myFunction3() {
+    document.getElementById("demo").innerHTML +=9;
+  }
+  function myFunction4() {
+    document.getElementById("demo").innerHTML +="*";
+  }
+  function myFunction5() {
+    document.getElementById("demo").innerHTML +=4;
+  }
+  function myFunction6() {
+    document.getElementById("demo").innerHTML +=5;
+  }
+  function myFunction7() {
+    document.getElementById("demo").innerHTML +=6;
+  }
+  
+  function myFunction8() {
+    document.getElementById("demo").innerHTML +="-";
+  }
+  function myFunction9() {
+    document.getElementById("demo").innerHTML +=1;
+  }
+  function myFunction10() {
+    document.getElementById("demo").innerHTML +=2;
+  }
+  function myFunction11() {
+    document.getElementById("demo").innerHTML +=3;
+  }
+  function myFunction12() {
+    document.getElementById("demo").innerHTML +="0";
+  }
+  function myFunction13() {
+    document.getElementById("demo").innerHTML +=".";
+  }
+  function myFunction14() {
+    document.getElementById("demo").innerHTML +=".";
+  }
+  function myFunction18() {
+    document.getElementById("demo").innerHTML +="/";
+  }
+  function myFunction15() {
+    let equalAmount=document.getElementById("demo").innerHTML;
+    document.getElementById("demo").innerHTML = eval(equalAmount);
+  
+  }
+  function myFunction16() {
+    document.getElementById("demo").innerHTML="";
+  }
+  function myFunction17() {
+  const value= document.getElementById("demo").innerHTML;
+  const myarray=Array.from(value);
+  myarray.pop();
+  let text=myarray.join("");
+  
+   document.getElementById("demo").innerHTML =text;
+  }
+  function myFunction19() {
+    document.getElementById("demo").innerHTML+="+";
+  }
+  
